cce2e4b847f36f208a9698d2fe5a6835e83a0fae
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-06-29 16:04:02 -0300

    feat: Home page.

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { CarList } from "../src/components/Carlist";
+import React from "react";
+
+function HomePage() {
+  return (
+    <React.StrictMode>
+      <CarList />
+    </React.StrictMode>
+  );
+}
+
+export default HomePage;
